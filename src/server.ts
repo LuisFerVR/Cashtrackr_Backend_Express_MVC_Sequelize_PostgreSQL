b76dba1739f5fdd63e5da197931cd3d70185b29b
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-07 12:21:51 -0600

    Configuración a BD PostgreSQL en Render.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.cyan.bold('DB is connected'))
+    } catch (error) {
+        console.log(colors.red.bold('DB connection error: '), error)
+    }
+}
+
+connectDB();
 
 const app = express()
 
