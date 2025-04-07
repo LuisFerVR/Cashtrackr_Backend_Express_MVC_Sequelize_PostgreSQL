b8147a28136e64158c40daedc816cee52eb4a8b7
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-07 13:37:32 -0600

    Configuración de modelos en db.ts y creación de primer modelo Budget.

---
+++ src/models/Budget.ts
@@ -0,0 +1,18 @@
+import {Table, Column, Model, DataType, HasMany, BelongsTo, ForeignKey } from 'sequelize-typescript';
+
+@Table({
+    tableName: 'budget',
+})
+
+class Budget extends Model {
+    @Column({
+        type: DataType.STRING(100)
+    })
+    name: string;
+    @Column({
+        type: DataType.DECIMAL
+    })
+    amount: number;
+}
+
+export default Budget; 
