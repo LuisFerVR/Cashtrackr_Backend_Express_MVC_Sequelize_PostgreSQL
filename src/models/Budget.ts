e9fdbd58dcc1a531fd7baf03b6564b54c166c318
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-09 17:07:42 -0600

    Creación y relación de la tabla users o modelo User y sus relaciones con Budget.

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,15 +1,19 @@
-import {Table, Column, Model, DataType, HasMany, BelongsTo, ForeignKey } from 'sequelize-typescript';
+import {Table, Column, Model, DataType, HasMany, BelongsTo, ForeignKey, AllowNull } from 'sequelize-typescript';
 import Expense from './Expense';
+import User from './User';
 
 @Table({
     tableName: 'budget',
 })
 
 class Budget extends Model {
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     declare name: string;
+    
+    @AllowNull(false)
     @Column({
         type: DataType.DECIMAL
     })
@@ -20,6 +24,12 @@ class Budget extends Model {
         onDelete: 'CASCADE'
     })
     declare expenses: Expense[];
+
+    @ForeignKey(() => User)
+    declare userId: number;
+
+    @BelongsTo(() => User)
+    declare user: User;
 }
 
 export default Budget; 
