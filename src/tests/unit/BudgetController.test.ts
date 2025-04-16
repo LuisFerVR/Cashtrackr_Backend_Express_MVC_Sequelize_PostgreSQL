5c64d3793c2219848d651e9686d22801bc9a82d5
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-16 14:38:47 -0600

    Construcción de test para recuperar los presupuestos por id de usuario.

--- src/tests/unit/BudgetController.test.ts
+++ src/tests/unit/BudgetController.test.ts
@@ -8,17 +8,57 @@ jest.mock("../../models/Budget",() => ({
 }))
 
 describe("BudgetController.getAll", () => {
-    it("should retrive 3 budgets",async () => {
+    it("should retrive 2 budgets for user with id 1",async () => {
         const req = createRequest({
             method: "GET",
             url: "/api/budgets",
-            user: {id:500}
+            user: {id:1}
         })
         const res = createResponse();
-        (Budget.findAll as jest.Mock).mockResolvedValue(budgets)
+        const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id);
+        (Budget.findAll as jest.Mock).mockResolvedValue(updatedBudgets)
         await BudgetController.getAll(req,res);
 
         const data = res._getJSONData()
         console.log(data);
+        expect(data).toHaveLength(2)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+    })
+
+    it("should retrive 1 budgets for user with id 2",async () => {
+        const req = createRequest({
+            method: "GET",
+            url: "/api/budgets",
+            user: {id:2}
+        })
+        const res = createResponse();
+        const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id);
+        (Budget.findAll as jest.Mock).mockResolvedValue(updatedBudgets)
+        await BudgetController.getAll(req,res);
+
+        const data = res._getJSONData()
+        console.log(data);
+        expect(data).toHaveLength(1)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+    })
+
+    it("should retrive 0 budgets for user with id 100",async () => {
+        const req = createRequest({
+            method: "GET",
+            url: "/api/budgets",
+            user: {id:100}
+        })
+        const res = createResponse();
+        const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id);
+        (Budget.findAll as jest.Mock).mockResolvedValue(updatedBudgets)
+        await BudgetController.getAll(req,res);
+
+        const data = res._getJSONData()
+        console.log(data);
+        expect(data).toHaveLength(0)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
     })
 })
