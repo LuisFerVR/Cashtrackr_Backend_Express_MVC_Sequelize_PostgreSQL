04284f0328f8e8a01e4bec8e77b72ab0c2ba6a8e
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-18 16:18:59 -0600

    Se creó el archivo y mock de pruebas para ExpenseController.

--- src/controllers/ExpenseController.ts
+++ src/controllers/ExpenseController.ts
@@ -1,12 +1,11 @@
-import type { Request, Response } from 'express'
+import type { Request, Response } from 'express';
 import Expense from '../models/Expense';
-import Budget from '../models/Budget';
 
 export class ExpensesController {
   
     static create = async (req: Request, res: Response) => {
        try {
-            const expense = new Expense(req.body);
+            const expense = await Expense.create(req.body);
             expense.budgetId = req.budget.id;
             await expense.save();
             res.status(201).json("Gasto Agregado Correctamente");
